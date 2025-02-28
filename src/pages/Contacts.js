92009c817bb5125a76f3da1e20fb619255f4e797
Cimminelli1982 <96242255+Cimminelli1982@users.noreply.github.com>
2025-02-28 15:52:24 +0000

    Create Contacts.js

---
+++ src/pages/Contacts.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout/Layout';
+
+const Contacts = () => {
+  return (
+    <Layout>
+      <h1>Contacts</h1>
+      <p>Contacts listing page is under development.</p>
+    </Layout>
+  );
+};
+
+export default Contacts;
